f7db373f8cddbcda72770d4b8762f018c9055365
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-01-15 15:37:53 +0100

    Lager en fjong måte å bruke Jwk på og 🚀

--- azure-token-client-default/src/main/kotlin/com/github/navikt/tbd_libs/azure/Factory.kt
+++ azure-token-client-default/src/main/kotlin/com/github/navikt/tbd_libs/azure/Factory.kt
@@ -3,6 +3,7 @@ package com.github.navikt.tbd_libs.azure
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.net.URI
 import java.net.http.HttpClient
 
@@ -23,9 +24,37 @@ fun createDefaultAzureTokenClient(
     ))
 }
 
+fun createJwkAzureTokenClient(
+    tokenEndpoint: URI,
+    clientId: String,
+    jwk: Map<String, String>,
+    httpClient: HttpClient = HttpClient.newHttpClient(),
+    objectMapper: ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
+): AzureTokenProvider {
+    return InMemoryAzureTokenCache(AzureTokenClient(
+        tokenEndpoint = tokenEndpoint,
+        clientId = clientId,
+        authMethod = AzureAuthMethod.Jwk(jwk, clientId, tokenEndpoint),
+        client = httpClient,
+        objectMapper = objectMapper
+    ))
+}
+
 fun createAzureTokenClientFromEnvironment(env: Map<String, String> = System.getenv()) =
     createDefaultAzureTokenClient(
         tokenEndpoint = URI(env.getValue("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT")),
         clientId = env.getValue("AZURE_APP_CLIENT_ID"),
         clientSecret = env.getValue("AZURE_APP_CLIENT_SECRET")
     )
+
+fun createJwkAzureTokenClientFromEnvironment(env: Map<String, String> = System.getenv()): AzureTokenProvider {
+    val objectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
+    val jwk: Map<String, String> = objectMapper.readValue(env.getValue("AZURE_APP_JWK"))
+
+    return createJwkAzureTokenClient(
+        tokenEndpoint = URI(env.getValue("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT")),
+        clientId = env.getValue("AZURE_APP_CLIENT_ID"),
+        jwk = jwk,
+        objectMapper = objectMapper
+    )
+}
