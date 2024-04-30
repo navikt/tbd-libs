d46a31de72b2e141b24cf91a967b8a058400abf8
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2024-04-30 10:23:57 +0200

    Nå kan vi jo bruke den Signed JWT-tingen i AzureClient

--- azure-token-client/build.gradle.kts
+++ azure-token-client/build.gradle.kts
@@ -4,6 +4,7 @@ val mockkVersion: String by project
 dependencies {
     api("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+    api(project(":signed-jwt"))
 
     testImplementation("io.mockk:mockk:$mockkVersion")
     testImplementation(project(":mock-http-client"))
