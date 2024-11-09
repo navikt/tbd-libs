ddc40e5d0b4f0d912deefa4c00251e208432233e
david steinsland <david@davidsteinsland.net>
2024-11-09 09:27:12 +0100

    bruker kafka-test-containeren

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -1,8 +1,7 @@
 val kafkaVersion = "3.8.0"
-val kafkaTestcontainerVersion = "1.20.1"
 
 dependencies {
     api("org.apache.kafka:kafka-clients:$kafkaVersion")
 
-    testImplementation("org.testcontainers:kafka:$kafkaTestcontainerVersion")
+    testImplementation(project(":kafka-test"))
 }
