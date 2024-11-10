7dc71effecce24d2d354f2e6d069c4b00a798e3d
david steinsland <david@davidsteinsland.net>
2024-11-10 20:48:44 +0100

    er Isolated-annotasjonen redningen min?
    > When a test class is run in isolation, no other test class is executed concurrently
    
    Det høres jo ut som noe man trenger, når testen skal teste å ta ALLE topicene!

--- kafka-test/src/test/kotlin/com/github/navikt/tbd_libs/test_support/TømmeBassengetTest.kt
+++ kafka-test/src/test/kotlin/com/github/navikt/tbd_libs/test_support/TømmeBassengetTest.kt
@@ -4,11 +4,13 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode.SAME_THREAD
+import org.junit.jupiter.api.parallel.Isolated
 import java.time.Duration
 
 // må kjøre testene i samme tråd fordi begge testene forventer å tømme _hele_ bassenget for topics,
 // og det gir ikke mening å kjøre testene samtidig
 @Execution(SAME_THREAD)
+@Isolated
 class TømmeBassengetTest {
     @Test
     fun `får exception om topic ikke er tilgjengelig innen timeout`() = runBlocking {
@@ -23,10 +25,7 @@ class TømmeBassengetTest {
             // lar det være 1 ledig topic igjen
             val topics = kafkaContainer.nyeTopics(MAX_TOPICS_SIZE - 1)
             // kan umulig gå OK siden det skal kun være én ledig tilkobling
-            // (på grunn av andre tester i samme modul kan i teorien okkupere den siste tilkoblingen må vi ha litt timeout.
-            //      men siden vi vet at timeout vil inntreffe (siden det ikke finnes to ledige tilkoblinger) så unngår vi å sette
-            //       et så høyt tall at testen tar evigheter å fullføre..)
-            assertThrows<RuntimeException> { kafkaContainer.nyeTopics(2, timeout = Duration.ofSeconds(10)) }
+            assertThrows<RuntimeException> { kafkaContainer.nyeTopics(2, timeout = Duration.ofMillis(10)) }
             val sisteTopic = assertDoesNotThrow { kafkaContainer.nyeTopics(1) }
             kafkaContainer.droppTopics(topics + sisteTopic)
         }
