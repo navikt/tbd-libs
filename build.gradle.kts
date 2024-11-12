16cf2599af6b843a52d12e887fb92938920ae89a
david steinsland <david@davidsteinsland.net>
2024-11-12 11:04:29 +0100

    bumper junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
-val junitJupiterVersion = "5.11.0"
+val junitJupiterVersion = "5.11.3"
 val mockkVersion = "1.13.13"
 val jacksonVersion = "2.18.1"
 val testcontainersVersion = "1.20.3"
